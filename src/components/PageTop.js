0953d967aae09cc3638736f09aef52b56bc6f510
yk-amarly-20 <y.k.83600260@icloud.com>
2021-03-07 11:23:10 +0900

    fix: 重たいから点滅少なくしたけどどうかな

--- src/components/PageTop.js
+++ src/components/PageTop.js
@@ -64,13 +64,13 @@ const Top = () => {
     <div className="card-parent" id="top-view">
       <div className="card-children">
         <div className="my-name">
-          <span className="neon_1">Y</span><span className="neon_2">u</span><span className="neon_1">t</span>
-          <span className="neon_3">o</span> <span className="neon_2">K</span><span>o</span><span className="neon_3">j</span><span>i</span>
-          <span className="neon_2">m</span><span>a</span ><span className="neon_1">'</span><span className="neon_2">s</span> <span className="neon_3">p</span><span>o</span><span>r</span>
-          <span className="neon_1">t</span><span>f</span><span className="neon_1">o</span><span className="neon_3">l</span><span className="neon_2">i</span><span>o</span>
+          <span>Y</span><span className="neon">u</span><span>t</span>
+          <span>o K</span><span>oj</span><span>i</span>
+          <span>ma</span ><span className="neon">'</span><span></span> <span>po</span><span>r</span>
+          <span>tf</span><span className="neon">o</span><span>lio</span>
         </div>
         <div className="job">
-          Software Engineer (last updated: 2020/09/21)
+          Software Engineer (last updated: 2021/03/07)
         </div>
       </div>
     </div>
